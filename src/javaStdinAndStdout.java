0f92a209cdeeaf4b66ee3e79f3e8a8ba9fcbe7ff
Charley <52578804+charl3y@users.noreply.github.com>
2023-09-24 12:22:26 -0700

    Added hackerrank problem Stdin and Stdout I

---
+++ src/javaStdinAndStdout.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class javaStdinAndStdout {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int myFirstInt = scanner.nextInt();
+        int mySecondInt = scanner.nextInt();
+        int myThirdInt = scanner.nextInt();
+        scanner.close();
+
+        System.out.println(myFirstInt);
+        System.out.println(mySecondInt);
+        System.out.println(myThirdInt);
+    }
+}
